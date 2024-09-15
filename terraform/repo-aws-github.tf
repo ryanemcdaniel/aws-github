7134b35bbb7810de93be81ab5ad98ac1ffb4e09c
Ryan McDaniel <58714710+ryanemcdaniel@users.noreply.github.com>
2024-09-15 01:50:02 -0500

    fix: disallow automerge (#2)
    * fix: disallow automerge
    
    * fix: disallow automerge

--- terraform/repo-aws-github.tf
+++ terraform/repo-aws-github.tf
@@ -4,6 +4,7 @@ resource "github_repository" "aws_github" {
   description                 = "github repo configurations with terraform"
   visibility                  = "public"
   license_template            = "mit"
+  allow_auto_merge            = false
   allow_squash_merge          = true
   squash_merge_commit_title   = "COMMIT_OR_PR_TITLE"
   squash_merge_commit_message = "COMMIT_MESSAGES"
@@ -43,15 +44,17 @@ resource "github_branch_protection" "aws_github_main" {
   repository_id           = github_repository.aws_github.node_id
   pattern                 = "main"
   required_linear_history = true
+  allows_deletions        = false
   allows_force_pushes     = false
   force_push_bypassers    = [data.github_user.ryanemcdaniel.node_id]
-  allows_deletions        = false
   required_pull_request_reviews {
     require_code_owner_reviews      = true
     required_approving_review_count = 1
     require_last_push_approval      = true
     dismiss_stale_reviews           = true
-    pull_request_bypassers          = [data.github_user.ryanemcdaniel.node_id]
+    restrict_dismissals             = true
+    dismissal_restrictions          = ["/${data.github_user.ryanemcdaniel.username}"]
+    pull_request_bypassers          = ["/${data.github_user.ryanemcdaniel.username}"]
   }
   required_status_checks {
     strict = true
