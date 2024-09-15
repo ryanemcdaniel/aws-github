da6ca3dd3d28f11ae62d81d6ee18d54f1bf95185
ryanemcdaniel <58714710+ryanemcdaniel@users.noreply.github.com>
2024-09-15 01:33:42 -0500

    chore: init

---
+++ terraform/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_version = "1.9.1"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.67.0"
+    }
+    github = {
+      source  = "integrations/github"
+      version = "6.3.0"
+    }
+  }
+  backend "s3" {
+    region               = "us-east-1"
+    dynamodb_table       = "tf-ryanemcdaniel-management-lock"
+    bucket               = "tf-ryanemcdaniel-management"
+    workspace_key_prefix = ""
+    key                  = "aws-github.tfstate"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  default_tags {
+    tags = {
+      comp = "org"
+      env  = "org"
+      git  = "git@github.com:ryanemcdaniel/aws-organization.git"
+    }
+  }
+}
+
+provider "github" {}
+
+data "github_user" "ryanemcdaniel" {
+  username = "ryanemcdaniel"
+}
---
+++ terraform/repo-aws-github.tf
@@ -0,0 +1,64 @@
+resource "github_repository" "aws_github" {
+  name = "aws-github"
+
+  description                 = "github repo configurations with terraform"
+  visibility                  = "public"
+  license_template            = "mit"
+  allow_squash_merge          = true
+  squash_merge_commit_title   = "COMMIT_OR_PR_TITLE"
+  squash_merge_commit_message = "COMMIT_MESSAGES"
+  allow_merge_commit          = false
+  allow_rebase_merge          = false
+  allow_update_branch         = true
+  delete_branch_on_merge      = true
+  archive_on_destroy          = true
+  has_issues                  = true
+  has_discussions             = false
+  has_projects                = false
+  has_wiki                    = false
+  has_downloads               = false
+  auto_init                   = true
+  vulnerability_alerts        = true
+  security_and_analysis {
+    secret_scanning {
+      status = "enabled"
+    }
+    secret_scanning_push_protection {
+      status = "enabled"
+    }
+  }
+}
+
+resource "github_branch" "aws_github_main" {
+  repository = github_repository.aws_github.name
+  branch     = "main"
+}
+
+resource "github_branch_default" "aws_github_main" {
+  repository = github_repository.aws_github.name
+  branch     = github_branch.aws_github_main.branch
+}
+
+resource "github_branch_protection" "aws_github_main" {
+  repository_id           = github_repository.aws_github.node_id
+  pattern                 = "main"
+  required_linear_history = true
+  allows_force_pushes     = false
+  force_push_bypassers    = [data.github_user.ryanemcdaniel.node_id]
+  allows_deletions        = false
+  required_pull_request_reviews {
+    require_code_owner_reviews      = true
+    required_approving_review_count = 1
+    require_last_push_approval      = true
+    dismiss_stale_reviews           = true
+    pull_request_bypassers          = [data.github_user.ryanemcdaniel.node_id]
+  }
+  required_status_checks {
+    strict = true
+  }
+}
+
+import {
+  id = "aws-github"
+  to = github_repository.aws_github
+}
